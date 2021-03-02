ff1313d9997bd927dca17b8a6e07e740f0f09b4c
KinXpeng <kinxpeng@163.com>
2021-03-02 23:13:14 +0800

    添加

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 引入ElementUI
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+Vue.use(ElementUI);
+// 引入scss样式文件
+import "./assets/scss/reset.scss";
 Vue.config.productionTip = false
 
 new Vue({
