076e89a56f43e94ef75f08a2256a2d345d1baf01
KinXpeng <kinxpeng@163.com>
2021-03-04 17:34:28 +0800

    布局

---
+++ src/components/avatar.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="avatar">
+    <div class="head_back">
+      <div class="head_img">
+        <img :src="require('../assets/imgs/head_img.jpeg')" alt="">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  methods:{
+  },
+  created(){
+  },
+};
+</script>
+
+<style lang='scss' scoped>
+.avatar{
+  height:25vh;
+  min-height: 150px;
+  max-height: 210px;
+  .head_back{
+    width:100%;
+    height:100%;
+    background:url('../assets/imgs/blog_xiamu.jpeg') center/cover;
+    .head_img{
+      position: relative;
+      top:6vw;
+      left:6vw;
+      overflow: hidden;
+      width:16vw;
+      height:16vw;
+      border-radius: 50%;
+      border:4px solid #fff;
+      box-shadow: 5px 5px 10px #aaa;
+      transition: .5s;
+      &:hover{
+        border:4px solid rgb(84, 190, 249);
+        transform: scale(.5);
+      }
+      img{
+        width:100%;
+        height:100%;
+      }
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,44 @@
 <template>
   <div class="home">
-    home
+    <el-container>
+      <el-aside width="28.5%">
+        <avatar></avatar>
+      </el-aside>
+      <el-container class="content">
+        <el-main>Main</el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
+import avatar from '../components/avatar.vue'
 
 export default {
+  components: { avatar },
   
 }
 </script>
+<style lang="scss" scoped>
+.home{
+  min-width:900px;
+  .el-aside{
+    height:100vh;
+  }
+  .content{
+    position: relative;
+    width:71.5%;
+    height:100vh;
+    background:url('../assets/imgs/timer.jpeg') center/cover;
+    .el-footer{
+      position:absolute;
+      right:0;
+      bottom: 0;
+      width:100%;
+      height:20%;
+      border:1px solid red;
+    }
+  }
+}
+</style>
