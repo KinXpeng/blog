7127bcbb0173f20f180a8c401ca1de81c09c1841
KinXpeng <kinxpeng@163.com>
2021-06-30 17:48:22 +0800

    theme

--- src/views/articleItemInfo.vue
+++ src/views/articleItemInfo.vue
@@ -307,7 +307,7 @@ export default {
       // @include background_color("background_color");
       /deep/ pre{
         font-size: 12px;
-        // @include background_color("background_color6");
+        @include background_color("background_color6");
         // .hljs{
         //   @include background_color("background_color6");
         // }
